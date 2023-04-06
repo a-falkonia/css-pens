0f1426a7b385fdf9a2a7ea47ccdb5fec8d9ad040
Anna <a.sokolova@stream-telecom.ru>
2023-04-06 17:36:24 +0300

    first commit

---
+++ src/constants/codepens.ts
@@ -0,0 +1,9 @@
+import type { Codepen } from "../types";
+
+export const CODEPENS: Codepen[] = [
+    {
+        id: '100days-2',
+        name: 'Menu Icon',
+        link: '//codepen.io/a-falkonia'
+    }
+]
---
+++ src/types/index.ts
@@ -0,0 +1,5 @@
+export interface Codepen {
+    id: string;
+    name: string;
+    link: string;
+}

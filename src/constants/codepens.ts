0d1e3036801eb065e0236dd86d14d66998032626
Anna <a.sokolova@stream-telecom.ru>
2023-04-07 16:08:27 +0300

    feat: codepen styles

--- src/constants/codepens.ts
+++ src/constants/codepens.ts
@@ -4,6 +4,6 @@ export const CODEPENS: Codepen[] = [
     {
         id: '100days-2',
         name: 'Menu Icon',
-        link: '//codepen.io/a-falkonia'
+        link: '//codepen.io/a-falkonia/embed/JjmjVyg/?theme-id=light&default-tab=result'
     }
 ]

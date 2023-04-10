d2e53236be49867f44035476025da517fa499213
Anna <a.sokolova@stream-telecom.ru>
2023-04-10 16:38:17 +0300

    feat: add BoxShadow codepen

--- src/constants/codepens.ts
+++ src/constants/codepens.ts
@@ -5,5 +5,10 @@ export const CODEPENS: Codepen[] = [
         id: '100days-2',
         name: 'Menu Icon',
         link: '//codepen.io/a-falkonia/embed/JjmjVyg/?theme-id=light&default-tab=result'
-    }
+    },
+    {
+        id: 'boxshadow-checkbox',
+        name: 'Checkbox',
+        link: '//codepen.io/a-falkonia/embed/YzJXNeO/?theme-id=light&default-tab=result'
+    },
 ]
